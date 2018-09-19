cfcb9b80a74218dfe0e2ea714d567f055137ed2a
AnshKapoor <ansh605@gmail.com>
2018-09-19 18:59:38 +0530

    Created push, pop and function to return top element

--- stack.java
+++ stack.java
@@ -13,3 +13,16 @@ public class StackUsingLL {
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
+public void push(int item) {
+		this.list.addFirst(item);
+	}
+
+	public int pop() throws Exception {
+		int rv = this.list.getFirst();
+		this.list.removeFirst();
+		return rv;
+	}
+
+	public int top() throws Exception {
+		return this.list.getFirst();
+	}
