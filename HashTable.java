9f62db39294ada1ec6360e1871602c953fac0fe6
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-10-16 22:41:12 +0530

    Added other constructors
    Default constructor for hashtable and parametrized constructor

--- HashTable.java
+++ HashTable.java
@@ -17,3 +17,17 @@ public class HashTable<K, V> {
 			return "{" + this.key + ":" + this.value + "}";
 		}
 	}
+private int size;
+	private LinkedList<HTPair>[] bucketArray;
+	public static final int DEFAULT_CAPACITY = 10;
+
+	public HashTable() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	public HashTable(int capacity) {
+		// TODO Auto-generated constructor stub
+		this.bucketArray = (LinkedList<HTPair>[]) new LinkedList[capacity];
+		this.size = 0;
+	}
+
